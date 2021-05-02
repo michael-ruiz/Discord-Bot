ca00716a060369495ae74641e253bda3359eedc1
Michael Ruiz <michaelruiz@Michaels-MacBook-Pro.local>
2021-05-02 03:23:20 -0400

    space

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ action_symbol = '*'
 async def on_message(message):
     if message.author == bot.user:
         return
-        
+
     if message.content.startswith(action_symbol + 'hello'):
         await message.channel.send('Hello!')
+        
     if message.content.startswith(action_symbol + 'wow'):
         await message.channel.send('https://tenor.com/view/owen-wilson-owen-wilson-amazing-wonderful-gif-6103373')
 
