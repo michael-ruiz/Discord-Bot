c72f421e905c60e5655fb1fdf2a0688c89143a79
Marko P <marko.p1211@gmail.com>
2021-05-02 03:12:24 -0400

    added cool wow gif

--- main.py
+++ main.py
@@ -11,5 +11,7 @@ async def on_message(message):
         
     if message.content.startswith(action_symbol + 'hello'):
         await message.channel.send('Hello!')
+    if message.content.startswith(action_symbol + 'wow'):
+        await message.channel.send('https://tenor.com/view/owen-wilson-owen-wilson-amazing-wonderful-gif-6103373')
 
 bot.run(os.getenv('TOKEN'))
