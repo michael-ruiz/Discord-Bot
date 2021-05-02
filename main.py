c7b2186d45a8ae85813aa570ef5329354716d234
Marko P <marko.p1211@gmail.com>
2021-05-02 04:04:53 -0400

    Fun facts!!!!!!!

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import discord
 import os
+import random
 
 bot = discord.Client()
 action_symbol = '*'
+
+def funFact():
+    fact = random.randint(0 , 1)
+    if fact == 0:
+        return 'AHHHHH!'
+    elif fact == 1:
+        return ':flushed:'
 
 @bot.event
 async def on_message(message):
@@ -14,5 +22,8 @@ async def on_message(message):
         
     if message.content.startswith(action_symbol + 'wow'):
         await message.channel.send('https://tenor.com/view/owen-wilson-owen-wilson-amazing-wonderful-gif-6103373')
+
+    if message.content.startswith(action_symbol + 'funFact'):
+        await message.channel.send(funFact())
 
 bot.run(os.getenv('TOKEN'))
