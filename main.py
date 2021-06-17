8ae37a0a7140f5fda26cb382d42fe37a257cf649
Marko P <marko.p1211@gmail.com>
2021-06-17 02:39:58 -0400

    added text to help function

--- main.py
+++ main.py
@@ -37,16 +37,14 @@ async def picsearch(ctx, arg):
 
 @client.command()
 async def help(ctx):
-    help = discord.Embed(title='Test', url='https://www.youtube.com/watch?v=dQw4w9WgXcQ', description='Haha hehe', color=0x4287f5)
+    help = discord.Embed(title='Help', url='https://www.youtube.com/watch?v=dQw4w9WgXcQ', description="Below is a list of all of this bot's commands", color=0x4287f5)
     help.set_thumbnail(url='https://www.oxigeno.fm/wp-content/uploads/2015/10/Ayudame-470X310-436x291.jpg')
-    help.add_field(name='poopy pants', value='This is crazy', inline=False)
-    help.add_field(name='bruh', value='bruh bruh bruh bruh bruh bruh bruh bruh bruh bruh bruh bruh', inline=False)
-    help.add_field(name='funfact', value='a' , inline=False)    
-    help.add_field(name='wow', value='a', inline=False)
-    help.add_field(name='ping', value='a', inline=False)
-    help.add_field(name='covid', value='a', inline=False)
-    help.add_field(name='picsearch', value='a', inline=False)
-    help.add_field(name='help', value='a', inline=False)
+    help.add_field(name='funfact', value='Displays a random fun fact' , inline=False)    
+    help.add_field(name='wow', value='Wow', inline=False)
+    help.add_field(name='ping', value="Shows the bot's ping", inline=False)
+    help.add_field(name='covid', value="Displays today's covid numbers in Canada, Ontario and London", inline=False)
+    help.add_field(name='picsearch', value='Search for a photo on Google. Type your search word after the command', inline=False)
+    help.add_field(name='help', value='Shows this message', inline=False)
     help.set_footer(text='Thanks for listening to my TEDTalk')
     await ctx.send(embed = help)
 
